f91f6b81954c190cc7a8ab10f519ead09de5e491
Atifkalwar <111851893+Atifkalwar@users.noreply.github.com>
2025-02-15 23:39:47 +0500

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to You", "Komi ki behn"],
+    strings: ["HAPPY", "BIRTHDAY!", "to You", "Motii"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
